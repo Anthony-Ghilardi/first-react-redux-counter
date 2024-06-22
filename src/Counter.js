e90bed8c24ad5b6f0b6be2e27a424b0174968ba3
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-06-22 14:30:28 -0700

    Added todo list

--- src/Counter.js
+++ src/Counter.js
@@ -1,31 +1,33 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { decrement, increment, incrementByAmount } from './features/counterSlice'
 
 function Counter() {
-    const count = useSelector((state) => state.value)
+    const count = useSelector((state) => state.counter.value)
     const dispatch = useDispatch()
     const [input, setInput] = useState(0)
 
-    const byAmountSubmit = (e) => {
+    const byAmount = (e) => {
         e.preventDefault()
         dispatch(incrementByAmount(Number(input)))
     }
 
     return (
         <div>
-            <h1>
-                {count}
-            </h1>
-            <button onClick={() => dispatch(increment())}>
+            <h1>{count}</h1>
+            <button
+                aria-label="Increment value"
+                onClick={() => dispatch(increment())}>
                 Increment
             </button>
-            <button onClick={() => dispatch(decrement())}>
+            <button
+                aria-label="Decrement value"
+                onClick={() => dispatch(decrement())}>
                 Decrement
             </button>
-            <form onSubmit={(e) => byAmountSubmit(e)}>
-                <input type='number' onChange={ (e) => setInput(e.target.value)}/>
-                <button type='submit'>Submit</button>
+            <form onSubmit={(e) => byAmount(e)}>
+                <input type="number" onChange={(e) => setInput(e.target.value)} />
+                <button type="submit">Submit</button>
             </form>
         </div>
     )
